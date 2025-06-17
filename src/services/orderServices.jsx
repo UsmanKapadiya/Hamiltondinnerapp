0e12c1e428b7242baef2e89c864020b53e9b4214
Usman Kapadiya <hirenk@intellidt.com>
2025-06-17 16:20:38 +0530

    Dynamic Reports Show and Table Structure Update

--- src/services/orderServices.jsx
+++ src/services/orderServices.jsx
@@ -1,12 +1,14 @@
 import requests from "./api.js";
 
 const OrderServices = {
- submitOrder: async (payload) => {
-    return requests.post(`/multi-order-update`,payload);
+  submitOrder: async (payload) => {
+    return requests.post(`/multi-order-update`, payload);
   },
-  getMenuData: async(room_id, date) =>{
-    
+  getMenuData: async (room_id, date) => {
     return requests.post(`order-list?room_id=${room_id}&date=${date}&type=1`)
+  },
+  getReportList: async (date) => {
+    return requests.post(`get-report-data?date=${date}`)
   }
 }
 export default OrderServices;
