e0cf64cb1bdddd233b1fb4cd8832290faf3cef0f
Usman Kapadiya <hirenk@intellidt.com>
2025-10-13 16:27:52 +0530

    Unused Code remove

--- src/App.jsx
+++ src/App.jsx
@@ -18,30 +18,6 @@ function App() {
     <div>
        <Outlet />
     </div>
-    // <ColorModeContext.Provider value={colorMode}>
-    //   <ThemeProvider theme={theme}>
-    //     <CssBaseline />
-    //     <ToggledContext.Provider value={values}>
-    //       <Box sx={{ display: "flex", height: "100vh", maxWidth: "100%" }}>
-    //         <SideBar />
-    //         <Box
-    //           sx={{
-    //             flexGrow: 1,
-    //             display: "flex",
-    //             flexDirection: "column",
-    //             height: "100%",
-    //             maxWidth: "100%",
-    //           }}
-    //         >
-    //           <Navbar />
-    //           <Box sx={{ overflowY: "auto", flex: 1, maxWidth: "100%" }}>
-    //             <Outlet />
-    //           </Box>
-    //         </Box>
-    //       </Box>
-    //     </ToggledContext.Provider>
-    //   </ThemeProvider>
-    // </ColorModeContext.Provider>
   );
 }
 
