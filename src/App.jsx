2ee0a522fd9f2afa2b5d7c78283f2b20978e5418
Usman Kapadiya <hirenk@intellidt.com>
2025-10-13 17:03:57 +0530

    Unused Files Remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React, { createContext, useState } from "react";
-import { Box, CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
-import { Navbar, SideBar } from "./scenes";
 import { Outlet } from "react-router-dom";
-import Home from "./scenes/Home";
-import { Room } from "@mui/icons-material";
-import RoomEnter from "./scenes/roomLogin";
+
 
 export const ToggledContext = createContext(null);
 
 function App() {
-  const [theme, colorMode] = useMode();
   const [toggled, setToggled] = useState(false);
   const values = { toggled, setToggled };
   
