e504558f31a06b86be4916220166843cf988f0d6
Usman Kapadiya <usmankapadiyaiant@gmail.com>
2026-04-17 16:12:13 +0530

    Api Url update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const instance = axios.create({
-   baseURL: import.meta.env.VITE_API_BASE_URL || `https://hamiltondinnerapp.redesign.intelligrp.com/api`, 
+   baseURL: import.meta.env.VITE_API_BASE_URL || `https://hamiltondinnerapp.redesign.intellimanagement.com/api`, 
   // baseURL: import.meta.env.VITE_API_BASE_URL || `http://hamiltondinnerapp.staging.intelligrp.com/api/`, //old url
   timeout: 50000,
   headers: {
